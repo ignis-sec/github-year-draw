903647dbb60fb158ab5d419129f09027b4a6491a
Ata Hakçıl <ahakcil@gmail.com>
2018-10-27 16:51:28 +0300

    Now supports non black and white images
    Now converts normal bmp to grayscale and can create multi-toned commit drawings.
    There is a sample file, art.bmp

--- github-draw.py
+++ github-draw.py
@@ -3,7 +3,8 @@ import datetime
 from PIL import Image
 import numpy as np
 import sys
-
+import cv2
+import math
 if len(sys.argv)==1:
 	print("Please run this script with a console command, passing a path to a monochrome bmp image.")
 	exit()
@@ -20,16 +21,17 @@ gitlog_start = last_Sunday - datetime.timedelta(364) #find the starting point of
 
 
 
-im = Image.open(str(sys.argv[1])) #convert monochrome bpm to bool array
-p = np.array(im)
-p = p.transpose()
+p = cv2.imread(sys.argv[1],0)
+#cv2.imshow('image',p)
+p=p.transpose()
 i=0
-
+print(p)
 with open("temp.txt", "a") as myfile:
 	for x in range(0, 52):
 		for y in range(0,7):
-			for n in range(0,15): #commit 15 times, we want a dark color
-				if p[x][y]==False: ##only on black pixels
+			if p[x][y]!=255:
+				for n in range(0,math.floor((255-p[x][y])/10)): #commit according to color
+					#print(n)
 					myfile.write("1") #need changes for github commit
 					myfile.flush()
 					call('git commit -a -m "Commit"' + str(i) + ' --date="'+ str(gitlog_start+datetime.timedelta(x*7+y)) +'"') #commit command
