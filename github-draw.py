edc25f1b778d025c0db6c92479545fcf576683e9
Ata <ahakcil@gmail.com>
2020-02-09 15:06:06 +0200

    Modernizing the tool
    Compability for non-standart inputs, non-monochrome,wrong-size etc.

--- github-draw.py
+++ github-draw.py
@@ -3,11 +3,20 @@ import datetime
 from PIL import Image
 import numpy as np
 import sys
-import cv2
+
 import math
+import os
+
+fail="[\033[91m+\033[0m]"
+succ="[\033[92m+\033[0m]"
+warn="[\033[93m+\033[0m]"
+info="[\033[94m+\033[0m]"
+
 if len(sys.argv)==1:
-	print("Please run this script with a console command, passing a path to a monochrome bmp image.")
+	print("Example Usage:")
+	print(f"python3 {os.path.basename(__file__)} <filename>")
 	exit()
+
 
 def last_sunday(d): #get last sunday
 	if d.weekday() == 6:
@@ -15,24 +24,34 @@ def last_sunday(d): #get last sunday
 	else: 
 		return d-datetime.timedelta(d.weekday()+1)
 
+#find the starting point of github yearly activity
 d = datetime.datetime.today()
 last_Sunday = last_sunday(d) 
-gitlog_start = last_Sunday - datetime.timedelta(364) #find the starting point of github yearly activity
+gitlog_start = last_Sunday - datetime.timedelta(364) 
 
+print(f"{info} Opening image {sys.argv[1]}")
+im = Image.open(sys.argv[1])
 
-
-p = cv2.imread(sys.argv[1],0)
-#cv2.imshow('image',p)
-p=p.transpose()
+print(f"{info} Image size is {im.size[0]}x{im.size[1]}")
 i=0
-print(p)
+if(im.size[0]!=52 or im.size[1]!=7):
+	print(f"{warn} This script requires a 52*7 image to work with. Do you wish to resize? (Y/N)")
+	confirm = input(">>>")
+	if(confirm=='Y' or confirm=='y'):
+		print(f"{info} Resizing image.")
+		pass
+	else:
+		print(f"{fail} Quitting.")
+		exit(1)
+
+exit()
 with open("temp.txt", "a") as myfile:
 	for x in range(0, 52):
 		for y in range(0,7):
-			if p[x][y]!=255:
+			if im[x][y]!=255:
 				for n in range(0,math.floor((255-p[x][y])/10)): #commit according to color
 					#print(n)
 					myfile.write("1") #need changes for github commit
 					myfile.flush()
-					call('git commit -a -m "Commit"' + str(i) + ' --date="'+ str(gitlog_start+datetime.timedelta(x*7+y)) +'"') #commit command
+					#call('git commit -a -m "Commit"' + str(i) + ' --date="'+ str(gitlog_start+datetime.timedelta(x*7+y)) +'"') #commit command
 					i=i+1
